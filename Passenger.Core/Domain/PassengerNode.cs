6f253d394111f0a65f2009131bcc0372797e8e53
Michał Okoński <m.okonski@sente.pl>
2018-04-14 13:48:19 +0200

    Proste interakcje międzydomenowe

--- Passenger.Core/Domain/PassengerNode.cs
+++ Passenger.Core/Domain/PassengerNode.cs
@@ -4,5 +4,18 @@ namespace Passenger.Core.Domain
     {
         public Node Node {get; protected set;}
         public Passenger Passenger { get; protected set; }
+
+        protected PassengerNode()
+        {            
+        }
+
+        protected PassengerNode( Passenger passenger, Node node)
+        {
+            Node = node;
+            Passenger = passenger;
+        }
+
+        public static PassengerNode Create( Passenger passenger, Node node )
+            => new PassengerNode( passenger, node );
     }
 }
