d8156ba2d10f900291dfc22ee345ee2b4a9edbdd
Michał Okoński <m.okonski@sente.pl>
2018-04-14 08:44:18 +0200

    dołożenie Autofac i rozszerzenie domyślnego kontenera IoC

--- Passenger.Api/Startup.cs
+++ Passenger.Api/Startup.cs
@@ -12,6 +12,8 @@ using Passenger.Infrastructure.Services;
 using Passenger.Infrastructure.Repositories;
 using Passenger.Core.Repositories;
 using Passenger.Infrastructure.Mappers;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 
 namespace Passenger.Api
 {
@@ -23,25 +25,40 @@ namespace Passenger.Api
         }
 
         public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; } // trzyma konfigurację naszej aplikacji, jakimi klasami implementować nasze interfejsy
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IUserService,UserService>();
             services.AddScoped<IUserRepository,InMemoryUserRepository>();
             services.AddSingleton(AutoMapperConfig.Initialize());
             services.AddMvc();
+
+            // Implementacja Autofac'a (IoC) dependency injection
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
+            loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+
+            // Jeżeli aplikacja się zatrzyma to wywołaj metodę Register i wywołaj na naszym kontenerze Dispose aby wyczyścić nieużytki
+            appLifetime.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
         }
     }
 }
